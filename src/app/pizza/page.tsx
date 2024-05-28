cb5eb3d2604920dc618d77d3fb66d753d92c6944
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-05-28 16:37:50 +0300

    routing

---
+++ src/app/pizza/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Pizza() {
+  return (
+    <div className="container flex flex-col items-center justify-center pt-20">
+      <h1 className="">Pizza</h1>
+    </div>
+  )
+}
