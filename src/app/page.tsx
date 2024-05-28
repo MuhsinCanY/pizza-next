57d2839754a0d4777a8e7fd90cc20f8e1349553c
MuhsinCanY <mcyilmaz1998@gmail.com>
2024-05-28 21:41:55 +0300

    home page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+    <main className="">
+      <div className="flex gap-3 flex-col items-center justify-start pt-20 w-full h-screen bg-cover bg-top bg-hero-pattern">
+        <h1 className="text-3xl font-[700] font-roboto">Teknolojik Yemekler</h1>
+        <h2 className="text-[70px] font-[100] font-barlow">KOD ACIKTIRIR</h2>
+        <h2 className="text-[70px] font-[100] font-barlow">PIZZA, DOYURUR</h2>
+        <button className="bg-pizza_yellow text-black font-[500] font-barlow rounded-full py-2 px-8">
+          <Link href="/pizza">ACIKTIM</Link>
+        </button>
+      </div>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'hero-pattern': "url('/mvp-banner.png')",
+      },
+      fontFamily: {
+        roboto: ['Roboto Condensed', 'normal'],
+        barlow: ['Barlow', 'normal'],
+      },
+      backgroundColor: {
+        pizza_yellow: '#FDC913',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
